185cf0b3e0c86ad58bf27444fa7926594fcf5600
sdv22 <sdv22@live.ru>
2018-09-09 14:11:34 +0300

    Rmove FP wrapper and build fingerprint service 2.1 for capricorn blobs

--- msm8996.mk
+++ msm8996.mk
@@ -189,9 +189,11 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom \
-    fingerprintd \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprint.msm8996
+
+#   fingerprintd \
+#   android.hardware.biometrics.fingerprint@2.0-service-custom \
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
